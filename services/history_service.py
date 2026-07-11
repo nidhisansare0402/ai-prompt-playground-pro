19991f79a12deea458c07ec7cf11f99ba13a9710
Nidhi Sansare <nidhisansare.sits.it@gmail.com>
2026-07-11 21:13:05 +0530

    Added History Storage Engine

--- services/history_service.py
+++ services/history_service.py
@@ -8,3 +8,26 @@ class HistoryService:
     def __init__(self):
         project_root = Path(__file__).parent.parent
         self.history_file = project_root / "data" / "history.json"
+
+    def load_history(self):
+        # Load history from the JSON file
+        try:
+            with self.history_file.open("r",encoding="utf-8") as file:
+                history = json.load(file)
+            return history
+
+        except FileNotFoundError:
+            print("History file not found.")
+            return []
+
+        except json.JSONDecodeError:
+            print("History file is corrupted.")
+            return []
+    
+    def save_history(self, record):
+        # Load history from json file append new record and overwrite the file back to json file
+        history = self.load_history()
+        history.append(record)
+
+        with self.history_file.open("w", encoding= "utf-8") as file:
+            json.dump(history, file, indent= 4, ensure_ascii=False)
